ed192828e1107ed7cd6ca28169afec3e55f293b4
Amir Hossein Shekari <vanenshi@gmail.com>
2026-03-30 14:12:41 +0200

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const set = new Set();
+        for (let num of nums) {
+            if (set.has(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
